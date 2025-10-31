b90dd3c872b4bf534ce36c4f4bef849848e31b05
Jacob McAllister <mcallisterj@westerntc.edu>
2025-10-31 09:49:27 -0500

    Update
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- MvcMovie/Controllers/BerryController.cs
+++ MvcMovie/Controllers/BerryController.cs
@@ -7,5 +7,21 @@ namespace MvcMovie.Controllers
     [ApiController]
     public class BerryController : ControllerBase
     {
+
+        [HttpGet("")]
+        public IActionResult BerryHello() { 
+            return Ok("Berry works!");
+        }
+
+        [HttpGet("{berryType}")]
+        public IActionResult BerryType(string berryType) {
+            return Ok($"Berry type: {berryType}");
+        }
+
+        [HttpPost("")]
+        public IActionResult CreateBerry(object information) {
+            return Ok($"Info you gave: {information}");
+        }
+
     }
 }
