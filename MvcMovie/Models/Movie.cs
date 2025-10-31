dea86583c59ddb440d696e5bd0c47594e8a07c33
Jacob McAllister <mcallisterj@westerntc.edu>
2025-10-31 10:16:16 -0500

    End of class
    Signed-off-by: Jacob McAllister <mcallisterj@westerntc.edu>

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -4,8 +4,11 @@ namespace MvcMovie.Models;
 
 public class Movie {
     public int Id { get; set; }
+    [Required]
     public string? Title { get; set; }
     public DateTime ReleaseDate { get; set; }
+    [Required]
     public string? Genre { get; set; }
+    [Required]
     public decimal Price { get; set; }
 }
